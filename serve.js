243d738833797399b74bf8276ec7c72aff1c7910
Martins <166885227+HeberMartins@users.noreply.github.com>
2025-12-10 16:17:13 -0300

    fix

--- serve.js
+++ serve.js
@@ -1,154 +1,169 @@
 import 'dotenv/config';
 import express from 'express';
 import mongoose from 'mongoose';
- 
+
 const app = express();
 app.use(express.json());
- 
-mongoose.connect(process.env.MONGODB_URI, { dbName: 'Aula' })
+
+mongoose.connect(process.env.MONGODB_URI, { dbName: 'Weather-Journal' })
     .then(() => console.log('Conectado ao MongoDB'))
     .catch(err => console.error('Erro na conexão:', err.message));
- 
+
 const usuarioSchema = new mongoose.Schema({
     user: { type: String, required: true, trim: true, minlength: 2 },
     senha: { type: String, required: true, trim: true },
     email: { type: String, required: true, trim: true }
 }, ({ collection: 'usuarios', timestamps: true }));
 const Usuario = mongoose.model('Usuario', usuarioSchema, 'usuarios');
- 
+
 const registros = new mongoose.Schema({
-    cidade: { type: String, required: true, trim: true, minlenght: 2 },
-    estado: { type: String, required: true, trim: true, maxlenght: 2 },
+    cidade: { type: String, required: true, trim: true, minlength: 2 },
+    estado: { type: String, required: true, trim: true, maxlength: 2 },
     data_observacao: { type: String, required: true },
     temperatura_c: { type: Number, required: true },
-    condicao: { type: String, required: true, trim: true, minlenght: 2 },
+    condicao: { type: String, required: true, trim: true, minlength: 2 },
     sensacao_termica: { type: Number, required: true },
     umidade_percentual: { type: Number, required: true },
-    notas_usuario: { type: String, required: true, minlenght: 2 }
+    notas_usuario: { type: String, required: true, minlength: 2 }
 }, ({ collection: 'registro', timestamps: true }));
 const Registros = mongoose.model('Registro', registros, 'registro');
- 
+
 app.get('/', (req, res) => res.json({ msg: 'API rodando' }));
- 
+
 //CRUD Usuario:
- 
+
 app.post('/usuarios', async (req, res) => {
     try {
         const usuario = await Usuario.create(req.body);
- 
+
         if (!usuario) {
             return res.status(400).json({ error: 'Usuário não encontrado' });
         }
- 
+
         res.status(201).json(usuario);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao criar um usuário' });
     }
 });
- 
+
 app.get('/usuarios', async (req, res) => {
     try {
         const usuarios = await Usuario.find();
- 
+
         if (!usuarios) {
             return res.status(400).json({ error: 'Usuário não encontrado' });
         }
- 
+
         res.json(usuarios);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar os dados do usuário' });
     }
 });
- 
+
 app.patch('/usuarios', async (req, res) => {
     try {
         const usuario = await Usuario.update(req.body);
- 
+
         if (!usuario) {
             return res.status(400).json({ error: 'Usuário não encontrado' });
         }
- 
+
         res.status(200).json(usuario);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao atualizar os dados do usuário' });
     }
 });
- 
+
 app.delete('/usuarios', async (req, res) => {
     try {
         const usuario = await Usuario.delete(req.body);
- 
+
         if (!usuario) {
             return res.status(400).json({ error: 'Usuário não encontrado' });
         }
- 
+
         res.status(200).json(usuario);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao deletar o usuário' });
     }
 });
- 
+
 //CRUD registros:
- 
+''
 app.post('/registros', async (req, res) => {
     try {
         const registro = await Registros.create(req.body);
- 
+
         if (!registro) {
             return res.status(400).json({ error: 'Registro não encontrado' });
         }
- 
+
         res.status(201).json(registro);
     } catch (error) {
         res.status(500).json(({ error: 'Erro ao criar um registro' }));
     }
- 
- 
+
+
 });
- 
+
 app.get('/registros', async (req, res) => {
     try {
         const registro = await Registros.find();
- 
+
         if (!registro) {
             return res.status(400).json({ error: 'Resgistro não encontrado' });
         }
- 
+
         res.json(registro);
     } catch (error) {
         res.status(500).json({ error: 'Erro ao buscar os dados da registro' });
     }
 });
- 
-app.patch('/registros', async (req, res) => {
+
+app.patch('/registros/:id', async (req, res) => {
     try {
-        const registro = await Registros.update(req.body);
- 
+        const id = req.params.id;
+        const novosDados = req.body;
+
+        const registro = await Registros.findByIdAndUpdate(id, novosDados, { new: true });
+
         if (!registro) {
-            return res.status(400).json({ error: 'Registro não encontrada' });
+            return res.status(404).json({ error: 'Registro não encontrado' });
         }
- 
+
         res.status(200).json(registro);
     } catch (error) {
-        res.status(500).json({ error: 'Erro ao atualizar os dados do registro' });
+        res.status(500).json({ error: 'Erro ao atualizar o registro' });
     }
 });
- 
-app.delete('/registros', async (req, res) => {
+
+app.delete('/registros/:id', async (req, res) => {
     try {
-        const registro = await Registros.delete(req.body);
- 
+        const id = req.params.id;
+
+        const registro = await Registros.findByIdAndDelete(id);
+
         if (!registro) {
-            return res.status(400).json({ error: 'Registro não encontrado' });
+            return res.status(404).json({ error: 'Registro não encontrado' });
         }
- 
-        res.status(200).json(registro);
+
+        res.status(200).json({ msg: "Deletado com sucesso" });
     } catch (error) {
         res.status(500).json({ error: 'Erro ao deletar o registro' });
     }
 });
- 
- 
+
+app.get('/registros/:id', async (req, res) => {
+    try {
+        const registro = await Registros.findById(req.params.id);
+        if (!registro) return res.status(404).json({error: "Não encontrado"});
+        res.json(registro);
+    } catch (e) {
+        res.status(500).json({error: "Erro"});
+    }
+});
+
+
 app.listen(process.env.PORT, () =>
     console.log(`Servidor rodando em http://localhost:${process.env.PORT}`)
 );
